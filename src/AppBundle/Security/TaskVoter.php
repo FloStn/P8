549c8ea3d7784c7d461ea4ee7caff88ac01e9af5
P6 <flo.stein9578@yahoo.fr>
2019-05-30 20:08:46 +0200

    Clearing TaskVoter.

--- src/AppBundle/Security/TaskVoter.php
+++ src/AppBundle/Security/TaskVoter.php
@@ -9,19 +9,16 @@ use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 
 class TaskVoter extends Voter
 {
-    // these strings are just invented: you can use anything
     const VIEW = 'view';
     const EDIT = 'edit';
     const DELETE = 'delete';
 
     protected function supports($attribute, $subject)
     {
-        // if the attribute isn't one we support, return false
         if (!in_array($attribute, [self::VIEW, self::EDIT, self::DELETE])) {
             return false;
         }
 
-        // only vote on Post objects inside this voter
         if (!$subject instanceof Task) {
             
             return false;
@@ -38,7 +35,6 @@ class TaskVoter extends Voter
             return false;
         }
 
-        // you know $subject is a Post object, thanks to supports
         /** @var Task $task */
         $task = $subject;
 
@@ -56,20 +52,15 @@ class TaskVoter extends Voter
 
     private function canView(Task $task, User $user)
     {
-        // if they can edit, they can view
         if ($this->canEdit($task, $user)) {
             return true;
         }
 
-        // the Task object could have, for example, a method isPrivate()
-        // that checks a boolean $private property
         return !$task->isPrivate();
     }
 
     private function canEdit(Task $task, User $user)
     {
-        // this assumes that the data object has a getOwner() method
-        // to get the entity of the user who owns this data object
         return $user === $task->getOwner();
     }
 
